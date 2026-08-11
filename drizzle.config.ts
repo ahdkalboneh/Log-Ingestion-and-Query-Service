9652ae12e0956adbba7a716b242455fb22f5c2b1
Ahd Kalboneh <ahd-kalboneh-ahd1@hotmail.com>
2026-08-11 16:00:17 +0300

    fix

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: "postgres://postgres:postgres@localhost:5433/logs_db",
   },
 });
